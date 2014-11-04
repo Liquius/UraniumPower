244b70e6a40369209ae2598d88da24773551170f
Liquius <liquius@gmail.com>
2014-11-04 23:29:57 +0000

    added more

--- nuclearstuff/prototypes/recipe/fluid-recipe.lua
+++ nuclearstuff/prototypes/recipe/fluid-recipe.lua
@@ -2,8 +2,8 @@ data:extend({
 
   {
     type = "recipe",
-    name = "uranium-hexafluoride-enrichment-ii",
-    category = "centrifuge",
+    name = "uranium-hexafluoride-ii",
+    category = "chemistry",
     enabled = "true",
     energy_required = 5,
     ingredients =
@@ -19,5 +19,165 @@ data:extend({
     icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-ii.png",
     subgroup = "fluid",
     order = "a[oil-processing]-a[basic-oil-processing]"
-  }
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-ii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-i", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-ii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-depleted", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-ii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-ii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-i", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-ii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iiii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-iii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiiiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-iiii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiiiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiiiiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-iiiii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiiiiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiiiiiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiiii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiiiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-iiiiii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiiiiiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
+
+  {
+    type = "recipe",
+    name = "uranium-hexafluoride-iiiiiiiii",
+    category = "centrifuge",
+    enabled = "true",
+    energy_required = 5,
+    ingredients =
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiiiii", amount=25},
+    },
+    results=
+    {
+      {type="fluid", name="uranium-hexafluoride-iiiiiiiii", amount=10},
+      {type="fluid", name="uranium-hexafluoride-iiiiiii", amount=15}
+    },
+    icon = "__nuclearstuff__/graphics/icons/uranium-hexafluoride-iiiiiiiii.png",
+    subgroup = "fluid",
+    order = "a[oil-processing]-a[basic-oil-processing]"
+  },
 })
