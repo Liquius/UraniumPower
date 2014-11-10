a63569d3f41c645e7d6e17eb7dcab30970c7df32
Liquius <liquius@gmail.com>
2014-11-10 14:11:17 +0000

    control now works

--- NuclearStuff/control.lua
+++ NuclearStuff/control.lua
@@ -2,23 +2,35 @@
 require "defines"
  
 game.onevent(defines.events.onbuiltentity, function(event)
-	if event.createdentity.name == "nuclear-fission-reactor" then 
-		reactor = (event.createdentity)
+	if event.createdentity.name == "nuclear-fission-reactor" then
+		if glob.LReactor == nil then
+			glob.LReactor = {}
+		end
+		
+		reactor = {true}
+		
+		reactor[1] = event.createdentity
+		
+		
+		table.insert(glob.LReactor, reactor)
 	end
 end)
 
-fissionReactorOutput = {}
+
 
 game.onevent(defines.events.ontick, function(event)
-	if reactor == nil 
-		then game.player.print("no reactor")
-	elseif reactor.fluidbox[2] == nil 
-		then game.player.print("nil value from fluidbox")
-	else
-		fissionReactorOutput = reactor.fluidbox[2]
-		fissionReactorOutput["temperature"] = 100 
-		reactor.fluidbox[2] = fissionReactorOutput
-		game.player.print("should work")
+	if glob.LReactor ~= nil then
+		for k,LReactor in pairs(glob.LReactor) do
+			if LReactor[1].valid then
+				if LReactor[1].fluidbox[2] ~= nil then
+					fissionReactorOutput = {}
+					fissionReactorOutput = LReactor[1].fluidbox[2]
+					fissionReactorOutput["temperature"] = 100 
+					LReactor[1].fluidbox[2] = fissionReactorOutput
+					game.player.print("should work")
+				end
+			end
+		end
 	end
 end)
 
